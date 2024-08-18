0934dbbe812cdd0c116539c66c4d1f35cd4a733e
Peri Sai Venkata Sudheer <peri.sudheer@ibm.com>
2024-08-18 20:56:09 +0530

    secondcommit

--- src/test/java/com/javatechie/SpringJenkinsApplicationTests.java
+++ src/test/java/com/javatechie/SpringJenkinsApplicationTests.java
@@ -1,13 +1,21 @@
 package com.javatechie;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringJenkinsApplicationTests {
-
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
 	@Test
 	void contextLoads() {
+		logger.info("Test case executing second commit");
+		assertEquals(true, true);
 	}
 
 }
